4a88b1705580c6d9d3e2669f77573c5f180b95cf
Leonardo lima <57497823+LeeoLima@users.noreply.github.com>
2026-06-22 11:02:23 -0300

    Calculadora.sh - Linux
    Arquivo executável da calculadora pelo Power Shel

---
+++ calculadora.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "--- Calculadora Simples ---"
+read -p "Digite o primeiro número: " n1
+read -p "Digite o segundo número" n2
+resultado=$((n1 + n2))
+echo "o resultado da soma é: $resultado"
+
